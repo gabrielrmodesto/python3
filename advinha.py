5f955ae7407cf57829296bc0c6b7994b23515144
gabrielrmodesto <gabrielrmodesto5@gmail.com>
2018-06-21 20:54:18 -0300

    Troca do while pelo for, break e continue adicionados

--- advinha.py
+++ advinha.py
@@ -7,17 +7,23 @@ numero_secreto = 42
 total_tentativas = 3
 rodada = 1
 
-while (rodada <= total_tentativas):
+#while (rodada <= total_tentativas):
+for rodada in range(1,total_tentativas+1):
 	print("Tentativa: {} de {} ".format(rodada,total_tentativas))
-	chute_str = input("Digite o seu chute: ")
+	chute_str = input("Digite um numero entre 1 e 100: ")
 	print("Voce chutou ", chute_str)
 	chute = int(chute_str)
 	acertou =  chute == numero_secreto
 	maior = chute > numero_secreto
 	menor = chute < numero_secreto
 
+	if(chute < 1 or chute > 100):
+		print("Por favor digite um numero entre 1 e 100")
+		continue
+
 	if(acertou):
 		print("Voce acertou")
+		break
 	else:
 		print("Voce errou")
 		if(maior):
@@ -25,4 +31,4 @@ while (rodada <= total_tentativas):
 		elif(menor):
 			print("O numero secreto eh maior que seu chute")
 
-	rodada += 1
+#	rodada += 1
