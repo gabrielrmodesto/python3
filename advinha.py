3857cd3d58de87babf7fc88d1d861a7da7dddaa6
gabrielrmodesto <gabrielrmodesto5@gmail.com>
2018-06-21 20:15:02 -0300

    variaveis booleanas adicionadas

--- advinha.py
+++ advinha.py
@@ -7,8 +7,15 @@ numero_secreto = 42
 chute_str = input("Digite o seu chute: ")
 print("Voce chutou ", chute_str)
 chute = int(chute_str)
+acertou =  chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
 
-if(chute == numero_secreto):
+if(acertou):
 	print("Voce acertou")
 else:
 	print("Voce errou")
+	if(maior):
+		print("O numero secreto eh menor que seu chute")
+	elif(menor):
+		print("O numero secreto eh maior que seu chute")
