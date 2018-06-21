501b465b88fbbb4c324c78b27043013063af097c
gabrielrmodesto <gabrielrmodesto5@gmail.com>
2018-06-21 20:29:35 -0300

    While adicionado a logica do jogo, com tentativas e rodadas

--- advinha.py
+++ advinha.py
@@ -4,18 +4,25 @@ print("*********************************")
 
 numero_secreto = 42
 
-chute_str = input("Digite o seu chute: ")
-print("Voce chutou ", chute_str)
-chute = int(chute_str)
-acertou =  chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+total_tentativas = 3
+rodada = 1
 
-if(acertou):
-	print("Voce acertou")
-else:
-	print("Voce errou")
-	if(maior):
-		print("O numero secreto eh menor que seu chute")
-	elif(menor):
-		print("O numero secreto eh maior que seu chute")
+while (rodada <= total_tentativas):
+	print("Tentativa: {} de {} ".format(rodada,total_tentativas))
+	chute_str = input("Digite o seu chute: ")
+	print("Voce chutou ", chute_str)
+	chute = int(chute_str)
+	acertou =  chute == numero_secreto
+	maior = chute > numero_secreto
+	menor = chute < numero_secreto
+
+	if(acertou):
+		print("Voce acertou")
+	else:
+		print("Voce errou")
+		if(maior):
+			print("O numero secreto eh menor que seu chute")
+		elif(menor):
+			print("O numero secreto eh maior que seu chute")
+
+	rodada += 1
