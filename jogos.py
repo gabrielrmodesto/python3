24da1fac4649c8468bd5fe4d0309e314c9dab8d3
gabrielrmodesto <gabrielrmodesto5@gmail.com>
2018-06-28 15:56:26 -0300

    Arquivo com a escolha do jogo, entre advinha e forca

---
+++ jogos.py
@@ -0,0 +1,15 @@
+import forca
+import advinha
+
+print("*********************************")
+print("********Escolha o seu jogo*******")
+print("*********************************")
+print("\n")
+print("(1) Advinha - (2) Forca")
+
+escolha = int(input("Qual o seu jogo?"))
+
+if(escolha == 1)
+	print("Jogando advinha")
+elif(escolha == 2)
+	print("Jogando forca")
