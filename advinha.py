a6d8e4d571c62f8df405c9e6c4a74a13c04f6e09
gabrielrmodesto <gabrielrmodesto5@gmail.com>
2018-06-28 14:51:32 -0300

    Jogo

--- advinha.py
+++ advinha.py
@@ -4,7 +4,7 @@ print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
-numero_secreto = random.randrange(1,101)
+numero_secreto = random.randrange(1,101)	
 
 total_tentativas = 3
 rodada = 1
