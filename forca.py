4409141e8d1eb65e8684a9fbe1ba24c56ffb05c0
GabrielRM <gabrielrmodesto5@gmail.com>
2018-07-16 17:45:56 -0300

    Lista de letras acertadas

--- forca.py
+++ forca.py
@@ -4,19 +4,23 @@ def jogar():
 	print("*********************************")
 
 	palavra_secreta = "banana"
+    letras_acertadas = ["_","_","_","_","_","_"]
 
 	enforcou = False
 	acertou = False
 
+    print(letras_acertadas)
 	while(not enforcou and not acertou):
 		chute = input("Digite uma letra ")
 		chute = chute.strip()
 
 		posicao = 0
 		for letra in palavra_secreta:
-			if(chute.upper() == letra.upper()): 
-				print("A letra {} foi encontrada na posicao {} ".format(letra,posicao))
+			if(chute.upper() == letra.upper()):
+                letras_acertadas[posicao] = letra
+#				print("A letra {} foi encontrada na posicao {} ".format(letra,posicao))
 			posicao = posicao + 1
+        print(letras_acertadas)
 
 
 	print("Fim de jogo")
