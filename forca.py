0132767c30f52e349636874c29175a8eaa6a3173
gabrielrmodesto <gabrielrmodesto5@gmail.com>
2018-07-16 11:23:02 -0300

    Funcao strip, upper para tratar entradas

--- forca.py
+++ forca.py
@@ -3,6 +3,22 @@ def jogar():
 	print("Bem vindo ao jogo da Forca!")
 	print("*********************************")
 
+	palavra_secreta = "banana"
+
+	enforcou = False
+	acertou = False
+
+	while(not enforcou and not acertou):
+		chute = input("Digite uma letra ")
+		chute = chute.strip()
+
+		posicao = 0
+		for letra in palavra_secreta:
+			if(chute.upper() == letra.upper()): 
+				print("A letra {} foi encontrada na posicao {} ".format(letra,posicao))
+			posicao = posicao + 1
+
+
 	print("Fim de jogo")
 
 if(__name__ == "__main__"):
