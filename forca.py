5e284b1198e02dc8469d7ebc68cd7affa3542379
gabrielrmodesto <gabrielrmodesto5@gmail.com>
2018-07-22 14:07:30 -0300

    Logica do acerto e compreensao de lista para iniciar a palavra

--- forca.py
+++ forca.py
@@ -3,25 +3,35 @@ def jogar():
 	print("Bem vindo ao jogo da Forca!")
 	print("*********************************")
 
-	palavra_secreta = "banana"
-    letras_acertadas = ["_","_","_","_","_","_"]
+	palavra_secreta = "banana".upper()
+	letras_acertadas = ["_" for letra in palavra_secreta]
 
 	enforcou = False
 	acertou = False
+	erros = 0
 
-    print(letras_acertadas)
+	print(letras_acertadas)
 	while(not enforcou and not acertou):
 		chute = input("Digite uma letra ")
-		chute = chute.strip()
+		chute = chute.strip().upper()
 
-		posicao = 0
-		for letra in palavra_secreta:
-			if(chute.upper() == letra.upper()):
-                letras_acertadas[posicao] = letra
-			posicao = posicao + 1
-        print(letras_acertadas)
+		if(chute in palavra_secreta):
+			posicao = 0
+			for letra in palavra_secreta:
+				if(chute == letra):
+					letras_acertadas[posicao] = letra
+				posicao = posicao + 1
+		else:
+			erros += 1
 
+		enforcou = erros == 6
+		acertou = "_" not in letras_acertadas
+		print(letras_acertadas)
 
+	if(acertou):
+		print("Voce ganhou")
+	else:
+		print("Voce errou")
 	print("Fim de jogo")
 
 if(__name__ == "__main__"):
